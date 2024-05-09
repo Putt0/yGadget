c000645ce52d6061766e39b10535456793217ec9
Reinaldy Oliveira <reinaldymc4@gmail.com>
2024-05-09 15:34:43 -0300

    Adicionando outras informações em ServerListeners

--- src/main/java/github/com/putt0/listener/ServerListeners.java
+++ src/main/java/github/com/putt0/listener/ServerListeners.java
@@ -1,55 +1,26 @@
 package github.com.putt0.listener;
 
-import github.com.putt0.item.ItemBuilder;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
-import java.util.Random;
-
+@SuppressWarnings("all")
 public class ServerListeners implements Listener {
 
-    private final String[] BEM_VINDO_DE_VOLTA = new String[]{
-            "§bÉ muito bom ter você de volta!",
-            "§cSenti sua falta :(",
-            "§eVamos brincar?"
-    };
-
     @EventHandler
-    public void onPlayerJoinEvent(PlayerJoinEvent event) {
-        event.setJoinMessage(null);
-
-        Player player = event.getPlayer();
-
-        player.getInventory().setItem(4, new ItemBuilder().newItem(Material.CHEST, "§aGadget's", new String[]
-                {}, 1,(byte) 0));
-
-        Bukkit.getOnlinePlayers().forEach(players -> {
-
-            players.sendMessage("§8[§b+§8] §6" + player.getName() + " acaba de conectar!");
-            players.playSound(players.getLocation(), Sound.ARROW_HIT, 100f, 100f);
-
-            player.sendTitle("§6Olá " + player.getName(), player.hasPlayedBefore() ? BEM_VINDO_DE_VOLTA[new Random()
-                    .nextInt(BEM_VINDO_DE_VOLTA.length)] : "§bSeja bem-vindo!");
-        });
+    public void onBlockBreakEvent(BlockBreakEvent event) {
+        event.setCancelled(true);
     }
 
     @EventHandler
-    public void onPlayerQuitEvent(PlayerQuitEvent event) {
-        event.setQuitMessage(null);
+    public void onBlockPlaceEvent(BlockPlaceEvent event) {
+        event.setCancelled(true);
+    }
 
-        Player player = event.getPlayer();
-
-        Bukkit.getOnlinePlayers().forEach(players -> {
-
-            players.sendMessage("§8[§c-§8] §6" + player.getName() + " acabou de sair!");
-            players.playSound(players.getLocation(), Sound.ARROW_HIT, 100f, 100f);
-
-        });
+    @EventHandler
+    public void onPlayerDropItemEvent(PlayerDropItemEvent event) {
+        event.setCancelled(true);
     }
 }
